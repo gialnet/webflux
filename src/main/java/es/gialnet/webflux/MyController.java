967f11e8842920234654901ada7625b324b375c6
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2017-12-28 21:04:39 +0000

    firt commit

---
+++ src/main/java/es/gialnet/webflux/MyController.java
@@ -0,0 +1,18 @@
+package es.gialnet.webflux;
+
+import org.reactivestreams.Publisher;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import reactor.core.publisher.Mono;
+
+@Controller
+public class MyController {
+
+  @GetMapping("/")
+  @ResponseBody
+  public Publisher<String> handler() {
+      return Mono.just("Hello world! Pepe");
+  }
+}
